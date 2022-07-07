7470b1432da07d0823628713e4c3b1a0c8724cdb
saket1990 <saketkumarsunny@gmail.com>
2022-07-07 19:37:48 +0530

    change

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -16,7 +16,6 @@ const createBook = async function(req,res){
     }
     // extract parameters
     const { title,excerpt,userId,ISBN,category,subcategory,releasedAt} = requestBody; //destructuring 
-
     if (!validator.isValid(title)) {
         return res.status(400).send({ status: false, messege: "Title is required" });
     }
@@ -50,8 +49,7 @@ const createBook = async function(req,res){
     if (!validator.isValid(ISBN)) {
         return res.status(400).send({ status: false, messege: "ISBN is required" });
     }
-    if(!validator.isValidIsbn(ISBN)){
-        return res.status(400).send({status:false, msg:"Invalid ISBN"})}
+    if(!validator.isValidIsbn(ISBN)){return res.status(400).send({status:false, msg:"Invalid ISBN"})}//TA QUESTIONS
     const isbn = await bookModel.findOne({ISBN: ISBN})
     if(isbn){return res.status(400).send({status:false,msg:"Duplicate ISBN"})}
 
@@ -59,13 +57,11 @@ const createBook = async function(req,res){
     if (!validator.isValid(category)) {
         return res.status(400).send({ status: false, messege: "category is required" });
     }
-    // if (!validator.isValid(subcategory)) {
-    //     return res.status(400).send({ status: false, messege: "subcategory is required" });
-    // }
-  console.log(typeof subcategory)
-if(!validator.isValidSubcategory(subcategory)){return res.status(400).send({status:false, msg:"subcategory must be string or array"})}
+    if (!validator.isValid(subcategory)) {
+        return res.status(400).send({ status: false, messege: "subcategory is required" });
+    }
     if (!validator.isValid(releasedAt)) {
-        return res.status(400).send({ status: false, message: "releasedAt is required" });
+        return res.status(400).send({ status: false, messege: "releasedAt is required" });
     }
 
     const book= await bookModel.create(requestBody)
@@ -73,23 +69,4 @@ if(!validator.isValidSubcategory(subcategory)){return res.status(400).send({stat
     res.status(201).send({status:true,message: "Book created successfully",data:book})
 
 }
-const getBooks = async function(req,res){
-    const query = req.query
-    // const {userId,category,subcategory}= req.query
-    console.log(query.userId)
-  
-    
-
-    // if(!mongoose.isValidObjectId(query.userId)){
-    //     return res.status(400).send({ status: false, messege: "userId is Invalid" });
-    //    }
-
-    const book = await bookModel.find({$and:[{isDeleted:false},query]}).select({_id:1,title:1,excerpt:1,userId:1,category:1,releasedAt:1,reviews:1})
-    if(book.length===0){return res.status(404).send({status:false,message:"No Books Found"})}
-    console.log(book.length)
-    // if(!query.userId){ 
-    //     return res.status(400).send({status:false,message:"userId is required in query param"})
-    // } ///USERID TA SESSION
-    res.status(200).send({status:true,message:"Books list", data: book})
-}
-module.exports={createBook,getBooks};
+module.exports={createBook};
