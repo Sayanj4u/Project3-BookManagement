e7405bf288d289ee0741a37d48a4f2f4db8a149a
Sahil Singh Baghel <102261017+sahil3487@users.noreply.github.com>
2022-07-07 15:12:21 +0530

    day2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -32,7 +32,7 @@ const bookSchema = new mongoose.Schema(
         reviews: {
             type: Number,
             default: 0
-         },
+        },
         deletedAt: {
             type: Date,
             default: null
